01375fa5d46821c7ddaabc552c0bf2729cba0794
Leifer Mendez <leifer33@gmail.com>
2019-07-04 13:06:20 +0200

    Ya casi paga

--- src/app/module/home/components/side-bar-cart/side-bar-cart.component.ts
+++ src/app/module/home/components/side-bar-cart/side-bar-cart.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { UtilsService } from '../../../../shared/services/util.service';
+import {UtilsService} from '../../../../shared/services/util.service';
 
 
 @Component({
@@ -10,18 +10,21 @@ import { UtilsService } from '../../../../shared/services/util.service';
 export class SideBarCartComponent implements OnInit {
   @Input() step: any = 1;
   public total: any;
+  public shipping: any = null;
+  public total_feed: any = null;
 
   constructor(private util: UtilsService) {
     this.util.refreshShoppingData.subscribe(data => {
       if (data) {
-        this.total = data['total_shop']
+        this.total = data['total_shop'];
+        this.shipping = data['total_shipping'];
+        this.total_feed = data['total_feed'];
       }
     });
   }
 
   ngOnInit() {
   }
-
 
 
 }
