f779225f319517f62bf21ddb57c1c8b1029c33c3
Leifer Mendez <leifer33@gmail.com>
2019-09-03 16:53:26 +0200

    fix P

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
   AH_token:'CMS2831F6A84E2',
   stripe_public_key: 'pk_live_6KCWvSfjNHesV09523rQtZfA',
   sentry_key: null,
-  google_provider: '1085674862738-9vijili3ggokr8kjk51bo9u8rik1q1m6.apps.googleusercontent.com',
-  fb_provider: '493119484360306',
+  google_provider: '137599264411-ldd4mmgd5gde7p77l9mgai1lqq7j4ufk.apps.googleusercontent.com',
+  fb_provider: '958746547789996',
   tawk: '5d5a5a5e77aa790be32f9052',
   intercom: 'zk835neg',
   country: 'MX'
