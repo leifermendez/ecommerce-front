845e4cabe2d9ba343070fd0f384bdcdefc70e3e8
Leifer Mendez <leifer33@gmail.com>
2020-05-06 17:26:58 +0200

    First

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,10 @@ export const environment = {
   endpoint: 'https://test.mercacomunidad.com/api/1.0',
   AH_url: '',
   AH_token: '',
-  stripe_public_key: 'pk_test_Wj915HLpr6PpdvzQMuzq8idv',
+  stripe_public_key: 'STRIPE_PK',
   sentry_key: null,
-  google_provider: '137599264411-ldd4mmgd5gde7p77l9mgai1lqq7j4ufk.apps.googleusercontent.com',
-  fb_provider: '290664481957869',
+  google_provider: 'GOOGLE_ID',
+  fb_provider: 'FB_ID',
   tawk: '',
   intercom: '',
   country: 'ES'
